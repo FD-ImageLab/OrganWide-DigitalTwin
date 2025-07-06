60eddb6f8cac9469eb814269b6fc3f9d47c888f7
ChengYanHuPI <114715003+ChengYanHuPI@users.noreply.github.com>
2025-07-06 19:47:27 +0800

    Delete scripts/000_correlation_analysis directory

--- scripts/000_correlation_analysis/mht.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-from scripts.utils.func import draw_mht
-
-
-data = pd.read_excel("data/mediation/mht.xlsx", sheet_name=None)
-
-for fid, df in data.items():
-    figname = "plot/med/mht/" + fid + ".png"
-    draw_mht(df, figname=figname)
